7f4b4b4a87ece8feef3dee8d0dbaf23375b3ea45
taghaoCode <taghao123@163.com>
2020-12-24 21:15:06 +0800

    初始化目录,并导入字体图标

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+// 导入全局样式
+import './assets/styles/index.css'
 Vue.config.productionTip = false
 
 new Vue({
